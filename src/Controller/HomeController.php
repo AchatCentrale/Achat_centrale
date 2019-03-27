4b80b17ab9e0ebe17af57ad806d4229298f0a52c
Jibe0123 <Jbagostin@gmail.com>
2019-03-27 18:43:52 +0100

    Replace header from handmade

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Users;
+use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,8 +13,44 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(Connection $connection)
     {
-        return $this->render('Home/index.html.twig');
+
+
+        $sqlCatParent = "SELECT CatID, CatTitre, CatLien, CatDescription FROM dbo.Categories WHERE CatSort > 0 AND CatIDParent = 0 ORDER BY CatSort";
+
+        $conn = $connection->prepare($sqlCatParent);
+        $conn->execute();
+        $catParent = $conn->fetchAll();
+
+
+        $sqlRayons = "SELECT CatID, CatTitre, CatLien, CatTitreReq, CatDescription
+                        FROM dbo.PARAM_FIXES
+                        INNER JOIN dbo.Categories ON PF_ESPACE_PRIVE = CatIDParent
+                        WHERE CatSort > 0
+                        ORDER BY CatSort";
+
+        $conn = $connection->prepare($sqlRayons);
+        $conn->execute();
+        $rayons = $conn->fetchAll();
+
+
+
+
+        return $this->render('Home/index.html.twig', [
+            "catParent" => $catParent,
+            "rayons" => $rayons,
+        ]);
     }
+
+
+    /**
+     * @Route("/bruneau", name="home_b")
+     */
+    public function bruneau()
+    {
+        return $this->render('Home/header.html.twig');
+    }
+
+
 }
