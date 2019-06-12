4a8400897a487ad44d1eb0c65895394c576c8975
Jibe0123 <Jbagostin@gmail.com>
2019-06-12 15:21:24 +0200

    remove dump

--- public/assets/js/Panier.js
+++ public/assets/js/Panier.js
@@ -1,6 +1,7 @@
-(async function(document) {
+(async function (document) {
 
-
+    let nombre_article_panier = $('#qty-panier-header');
+    let valeur_totale_cart = $('#total_price_cart');
 
 
     function postRequest(url, data) {
@@ -16,7 +17,6 @@
     }
 
 
-
     let btn_add = $('.btn_add_to_cart');
     let btn_remove_product = $('.remove-produit-panier');
 
@@ -24,16 +24,12 @@
     btn_add.on('click', function (e) {
 
 
-
         let price = $(this).data('price');
         let anchor = $(this).data('anchor');
         let produit_id = $(this).data('prid');
-        let quantity = $(anchor+" .p-quantity-input").val();
+        let quantity = $(anchor + " .p-quantity-input").val();
 
-        console.log(price);
-        console.log(anchor);
-        console.log(produit_id);
-        console.log(quantity);
+
         $(this).children('p').hide();
         $(this).children(':last').show();
 
@@ -41,17 +37,27 @@
         let url = "http://localhost:8000/cart/new";
 
         postRequest(url, {prid: produit_id, qty: quantity, prix: price})
-            .then(data =>{
+            .then(data => {
                 console.log(data);
                 $(this).children(':last').hide();
                 $(this).children('p').show();
+
+                console.log(parseFloat(valeur_totale_cart.text()));
+                console.log(price);
+
+                let new_value_amount = parseFloat(valeur_totale_cart.text()) + parseFloat(price);
+
+                console.log(new_value_amount);
+
+                nombre_article_panier.html(parseInt(nombre_article_panier.text()) + 1);
+                valeur_totale_cart.html(new_value_amount)
+
 
 
             }) // Result from the `response.json()` call
             .catch(error => console.error(error))
 
     });
-
 
 
     document.getElementById('header-cart').addEventListener('mouseleave', function () {
@@ -68,7 +74,6 @@
     });
 
 
-
     //remove un article du panier
     btn_remove_product.on('click', (e) => {
 
@@ -76,28 +81,34 @@
 
         const ptID = $(e.currentTarget).data('prid');
 
-        postRequest(url, {ptid : ptID})
+        postRequest(url, {ptid: ptID})
             .then(data => {
-                let qty_panier = $('#qty-panier-header');
 
-                const qty_panier_val = qty_panier.html();
+                const valeur_totale_cart_val = parseFloat(valeur_totale_cart.text());
 
+                const nombre_article_panier_val = parseFloat(nombre_article_panier.text());
 
-                const price = parseInt($(e.currentTarget).data('price'));
-                const total_price_cart = $('#total_price_cart');
-                const total_price_cart_val = parseInt($('#total_price_cart').text());
+                const price = $(e.currentTarget).data('price');
 
+                const new_valeur = valeur_totale_cart_val - price;
 
-                let result_remove_price = total_price_cart_val-price;
-                let qty_remove_val = qty_panier_val - 1;
-
-                qty_panier.html(qty_remove_val);
-                total_price_cart.html(result_remove_price);
+                const new_nombre_article_panier = nombre_article_panier_val - 1;
 
 
+                //Modif nombre article
+                const pastille_qty_cart_header = $('#qty-panier-header').html(new_nombre_article_panier);
+                const qty_cart_header_popup = $('#qty-panier-header-popup').html(new_nombre_article_panier);
+
+                //Modif valeur panier
+                const total_valeur_cart_header = valeur_totale_cart.html(new_valeur.toFixed(2));
+                const total_valeur_cart_popup = $('#total_price_cart_popup').html(new_valeur.toFixed(2));
+
+
+                console.log(price);
+                console.log(valeur_totale_cart_val);
 
                 console.log(data);
-                $('li#pt-'+ptID).remove();
+                $('li#pt-' + ptID).remove();
 
             }) // Result from the `response.json()` call
             .catch(error => console.error(error));
@@ -105,6 +116,6 @@
 
         console.log(e)
     })
-    
+
 
 })(document);
